39c6ec01577b93a080ab920c581f4bf9594113ef
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-10-03 18:51:14 +0300

    Exception

---
+++ src/com/company/CookIsBusyException.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class CookIsBusyException extends Exception{
+    public CookIsBusyException(int massage){
+        super("Sorry, but the cook is busy ");
+
+    }
+
+
+}
---
+++ src/com/company/Pizza.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Pizza {
+    Scanner sc = new Scanner(System.in);
+    public void call() {
+        System.out.println("Enter a number : ");
+        int numOfPizza = sc.nextInt();
+
+        if (numOfPizza<=5 ) {
+            System.out.println("We can make your order ...");
+        }else {
+            try {
+                 throw new CookIsBusyException(numOfPizza);
+            }  catch (CookIsBusyException e) {
+                System.out.println("The cook is busy :((( Yoy have ordered too many pizzas");
+            }
+        }
+    }
+}
